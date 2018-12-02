a316ddb88ede9780365452a515e0d0e40f480cc5
Ibáñez de Aldecoa Marin,Z.G. (Guillermo) <z.g.ibanezdealdecoamarin@students.uu.nl>
2018-12-02 12:03:14 +0100

    Tidied up classes

---
+++ shipSpace/Sensor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shipSpace
+{
+    class Sensor
+    {
+        public List<Error> errorList;
+        private Ship ship;
+
+        //Ideally I would like to pass a reference to the physical property of the ship.
+        //This way, the sensor would be completely independent of the property.
+        public enum SensorType { HEAD, VELOCITY};
+        public SensorType sensorType;
+
+        public Sensor(Ship ship, SensorType sensorType)
+        {
+            this.ship = ship;
+            this.sensorType = sensorType;
+
+            errorList = new List<Error>();
+        }
+
+        public double GetSensorValue()
+        {
+            double sensorValue = 0;
+
+            //Get the physical property
+            switch (sensorType)
+            {
+                case SensorType.HEAD:
+                    sensorValue = Ship.HeadToDegrees(ship.headX, ship.headY);
+                    break;
+                case SensorType.VELOCITY:
+                    sensorValue = ship.speed;
+                    break;
+                default:
+                    break;
+            }
+
+            //Add the sensor errors
+            foreach (Error error in errorList)
+            {
+                sensorValue += error.GetErrorValue();
+            }
+
+            return sensorValue;
+        }
+
+    }
+}
---
+++ shipSpace/UdpSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace shipSpace
+{
+    //This implementation is not entirely mine!
+    public class UdpSender
+    {
+        private string IP;
+        public int port;
+
+        IPEndPoint remoteEndPoint;
+        UdpClient client;
+
+        public UdpSender(int port)
+        {
+            IP = "127.0.0.1";
+            this.port = port;
+
+            remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+            client = new UdpClient();
+        }
+
+        public void sendString(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                client.Send(data, data.Length, remoteEndPoint);
+                Console.WriteLine("Sending message: " + message);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+    }
+}
